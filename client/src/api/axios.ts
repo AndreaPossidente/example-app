1045d1866d534ade13fd204f75192b7f473339e6
Andrea Possidente <ndrpssdnt@gmail.com>
2023-05-10 01:47:50 +0200

    intercept request to add bearer token

--- client/src/api/axios.ts
+++ client/src/api/axios.ts
@@ -3,14 +3,25 @@ import Cookies from "js-cookie";
 
 const { VITE_SERVER_URL = "http://localhost:4000" } = import.meta.env;
 
-const token = Cookies.get("jwt");
-
-axios.defaults.headers.common = { Authorization: `Bearer ${token}` };
-
-export default axios.create({
+const axiosClient = axios.create({
   baseURL: VITE_SERVER_URL,
   withCredentials: true,
   headers: {
     "Content-Type": "application/json",
   },
 });
+
+axiosClient.interceptors.request.use(
+  (config) => {
+    const token = Cookies.get("jwt");
+    if (token) {
+      config.headers["Authorization"] = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => {
+    Promise.reject(error);
+  }
+);
+
+export default axiosClient;
